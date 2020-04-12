44df68b52c44afa51357a279f7aaefd678d79bb2
TaeHyoungKwon <kth5604@gmail.com>
2020-04-12 21:25:46 +0900

    add is_korean func

--- utils.py
+++ utils.py
@@ -1,2 +1,9 @@
+import re
+
+
 def encode_euc_kr(search_word):
     return str(search_word.encode("euc-kr")).replace("\\x", "%")[3:-1].upper()
+
+
+def is_korean(text):
+    return len(re.findall(u"[\u3130-\u318F\uAC00-\uD7A3]+", text)) > 0
