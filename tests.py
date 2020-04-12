fb2e00fa33b9e0acc21b2a073f1b31b3a6653461
TaeHyoungKwon <kth5604@gmail.com>
2020-04-12 21:29:34 +0900

    refactor testcode

--- tests.py
+++ tests.py
@@ -13,49 +13,52 @@ class TestAladinCrawling(unittest.TestCase):
     @patch("crawling_aladin.convert_text_to_soup", Mock(return_value=BeautifulSoup("<html>None</html>", "html.parser")))
     @patch("crawling_aladin.request_get")
     def test_request_get_should_called_with_aladin_url_on_calling_aladin_url(self, mock_request_get):
+        # When: Call crawl_aladin
         crawl_aladin()
+
+        # Then: request_get should called with ALADIN_URL
         mock_request_get.assert_called_with(ALADIN_URL, None)
 
     @patch("crawling_aladin.convert_text_to_soup", Mock(return_value=BeautifulSoup("<html>None</html>", "html.parser")))
     @patch("crawling_aladin.request_get", Mock())
     def test_crawl_aladin_should_return_404_when_book_search_result_is_none(self):
+        # When: Call crawl_aladin
         response = json.loads(crawl_aladin())
 
+        # Then: should return 404 and NOT_FOUND
         self.assertEqual(response["status"], 404)
         self.assertEqual(response["message"], "NOT_FOUND")
 
     @patch("crawling_aladin.convert_text_to_soup", Mock(return_value=BeautifulSoup(BOOK_RESULT_HTML, "html.parser")))
     @patch("crawling_aladin.request_get", Mock())
     def test_crawl_aladin_should_return_200_when_book_search_result_is_exist(self):
+        # When: Call crawl_aladin
         response = json.loads(crawl_aladin())
 
+        # Then: should return 200 and OK
         self.assertEqual(response["status"], 200)
         self.assertEqual(response["message"], "OK")
 
     @patch("crawling_aladin.convert_text_to_soup", Mock(return_value=BeautifulSoup(BOOK_RESULT_HTML, "html.parser")))
     @patch("crawling_aladin.request_get", Mock())
     def test_crawl_aladin_should_return_book_result_txt_when_book_search_result_is_exist(self):
+        # When: Call crawl_aladin
         response = json.loads(crawl_aladin())
 
+        # Then: should return 200 and OK
         self.assertEqual(response["status"], 200)
         self.assertEqual(response["message"], "OK")
-
+        # And: should return result message about book list
         self.assertEqual(response["result"], (INFO_MESSAGE
                                               + BOOK_RESULT_TXT.format(str(2))
                                               + "1. 할랄, 신이 허락한 음식만 먹는다 (반양장)\n"
                                               + "2. 문화코드, 어떻게 읽을 것인가 (반양장)"))
 
     def test_is_korean_should_return_true_when_given_text_is_korean(self):
-        text = '한글123'
-        actual = is_korean(text)
-        self.assertEqual(actual, True)
+        self.assertEqual(is_korean('한글123'), True)
 
     def test_is_korean_should_return_false_when_given_text_is_not_korean(self):
-        text = 'python123'
-        actual = is_korean(text)
-        self.assertEqual(actual, False)
+        self.assertEqual(is_korean('python123'), False)
 
     def test_encode_euc_kr_on_korean(self):
-        search_word = '파이썬'
-        actual = encode_euc_kr(search_word)
-        self.assertEqual(actual, 'C6%C4%C0%CC%BD%E3')
+        self.assertEqual(encode_euc_kr('파이썬'), 'C6%C4%C0%CC%BD%E3')
